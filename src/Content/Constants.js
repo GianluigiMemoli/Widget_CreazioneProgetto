ca5fafc74fb251cc65bef85e8aec199a98849ece
Gianluigi Memoli <g.memoli98@gmail.com>
2021-09-22 21:07:16 +0200

    compatibilità nuovo sistema

--- src/Content/Constants.js
+++ src/Content/Constants.js
@@ -9,9 +9,18 @@ export const ADMIN = false;
 
 // Widget dimension
 export const isResizable = true;
-export const WIDGET_DEFAULT_DIMENSION =  { w: 3, h: 5};
-export const WIDGET_MAX_DIMENSION =  { w: 3, h: 6 };
+export const WIDGET_DEFAULT_DIMENSION =  { w: 5, h: 6};
+export const WIDGET_MAX_DIMENSION =  { w: 10, h: 10 };
 export const WIDGET_MIN_DIMENSION ={ w: 2, h: 5};
+
+// Widget style
+export const BACKGROUND = "#fff";
+
+// Widget configuration
+export const CONFIGURATION = [];
+
+// Default configuration
+export const DEFAULT_CONFIGURATION = {};
 
 // Author
 export const AUTHOR = "Gianluigi Memoli";
